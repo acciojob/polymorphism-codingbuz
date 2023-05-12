49cde3389700828737e6dd2e030f91aa3928badc
Upendra Kumar <124766145+codingbuz@users.noreply.github.com>
2023-05-12 23:31:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public Main(){}
     public static void main(String[] args) {
         Product p = new Product();
-//            System.out.println(p.product(2,4));
-//            System.out.println(p.product(2,4,5));
-//            System.out.println(p.product(2.0,4.0));
-        p.product(1, 2);
-        p.product(2, 3, 4);
-        p.product(1.0, 2.0);
+           System.out.println(p.product(2,4));
+           System.out.println(p.product(2,4,5));
+           System.out.println(p.product(2.0,4.0));
+//         p.product(1, 2);
+//         p.product(2, 3, 4);
+//         p.product(1.0, 2.0);
     }
         public static class Product{
 
